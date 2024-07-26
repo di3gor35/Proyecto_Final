fe9168d5b253640b86a19c434b7097c78d05cbd0
JMMendozaF <jair.mendoza.floresxd@gmail.com>
2024-07-25 23:15:15 -0500

    2do Commit: Se implementa el menú principal

--- utilidades.cpp
+++ utilidades.cpp
@@ -4,53 +4,21 @@
 #include "utilidades.h"
 using namespace std;
 
-void gestpac (){       
-	int opc;
-	do {
-		cout<<"----- GESTIÓN DE PACIENTES -----"<<endl;
-		cout<<"1) Agregar nuevo paciente"<<endl;
-		cout<<"2) Eliminar paciente"<<endl;
-		cout<<"3) Actualizar información de paciente"<<endl;
-		cout<<"4) Buscar paciente por número de identificación"<<endl;
-		cout<<"5) Volver al menú principal"<<endl;
-		cout<<"Seleccione una opción: ";
-		cin>>opc;
-		
-		switch (opc){
-			case 1:
-				agregar();
-				break;
-			case 2:
-				eliminar();
-				break;
-			case 3:
-				actualizar();
-				break;
-			case 4:
-				buscar();
-				break;
-			case 5:
-				menuprin();
-				break;
-			default:
-				cout<<"Opción no válida. Pruebe otra vez: ";
-		}
-	} while (opc!=manuprinc);
-}
-
-struct Paciente{
-	string nombre;
-	int edad;
-	float promedio;
+void menuprinc (){
+	int op;
+	cout<<"----- MENÚ PRINCIPAL -----"<<endl;
+	cout<<"1) Gestión de Pactientes"<<endl;
+	cout<<"2) Visualización de Datos"<<endl;
+	cout<<"3) Análisis de Datos"<<endl,
+	cout<<"4) Funcionalidades Específicas"<<endl;
+	cout<<"5) Cerrar el programa"<<endl;
+	cout<<"Elija su opción: ";
+	cin>>op;
 };
-
-void agregar (){
-	
-}
 
 int main (){
 	SetConsoleOutputCP(CP_UTF8);
-	gestpac ();
-	
+	menuprinc ();
+		
 	return 0;
 }
