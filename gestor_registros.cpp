49c044245ffcfc25d3da38b60efd6ca3b3f0201a
di3go <djramosm@unjbg.edu.pe>
2024-07-28 16:41:57 -0500

    Se agregaron las funciones buscar, actualizar y eliminar paciente

--- gestor_registros.cpp
+++ gestor_registros.cpp
@@ -12,12 +12,74 @@ void agregarPaciente(RegistroPacientes &registro, Paciente paciente){
 		registro.pacientes[registro.numPacientes] = paciente;
 		registro.numPacientes++;
 	} else {
-		cout << "Se alcanzo el limite del registro para el guardado del paciente" << endl; 
+		cout << "CAPACIDAD MAXIMA: Se alcanzo los 100 pacientes en el registro" << endl; 
 	}
 }
 
 void listarPacientes(RegistroPacientes registro){
 	for (int i = 0; i < registro.numPacientes; i++){
-		cout << "Nombre: " << registro.pacientes[i].nombre << ", edad: " << registro.pacientes[i].edad << endl;
+		cout << "Paciente " << i+1 << endl;
+		cout << "Nombre: " << registro.pacientes[i].nombre << endl;
+        cout << "Edad: " << registro.pacientes[i].edad << endl;
+        cout << "Genero: " << registro.pacientes[i].genero << endl;
+        cout << "DNI: " << registro.pacientes[i].numeroDNI << endl;
+        cout << "Tipo de Sangre: " << registro.pacientes[i].tipoSangre << endl;
+        cout << "Historial Médico: ";
+		for (int j = 0; j < registro.pacientes[i].numEntradasHistorial; j++){
+			cout << registro.pacientes[i].historialMedico[j] << " ";
+		}
+		cout << endl;
 	}
 }
+
+void buscarPaciente(RegistroPacientes registro, string numeroDNI){
+	for (int i = 0; i < registro.numPacientes; i++){
+		if (registro.pacientes[i].numeroDNI == numeroDNI){
+			cout << "Paciente encontrado" << endl;
+			cout << "Nombre: " << registro.pacientes[i].nombre << endl;
+            cout << "Edad: " << registro.pacientes[i].edad << endl;
+            cout << "Genero: " << registro.pacientes[i].genero << endl;
+            cout << "DNI: " << registro.pacientes[i].numeroDNI << endl;
+            cout << "Tipo de Sangre: " << registro.pacientes[i].tipoSangre << endl;
+            cout << "Historial Médico: ";
+			for (int j = 0; j < registro.pacientes[i].numEntradasHistorial; j++){
+				cout << registro.pacientes[i].historialMedico[j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	cout << "Paciente no encontrado" << endl;
+}
+
+void actualizarPaciente(RegistroPacientes &registro, string numeroDNI){
+	for (int i = 0; i < registro.numPacientes; i++){
+		if (registro.pacientes[i].numeroDNI == numeroDNI){
+			cout << "Paciente encontrado" << endl;
+			cout << "Introduzca los nuevos datos:" << endl;
+			cout << "Nombre: ", getline(cin, registro.pacientes[i].nombre)<< endl;
+            cout << "Edad: ", cin >> registro.pacientes[i].edad << endl;
+            cout << "Genero: ", cin >> registro.pacientes[i].genero << endl;
+            cout << "Tipo de Sangre: ", cin >> registro.pacientes[i].tipoSangre << endl;
+            cout << "Historial Médico: ";
+			for (int j = 0; j < registro.pacientes[i].numEntradasHistorial; j++){
+				cout << j+1 << ": ", cin >> registro.pacientes[i].historialMedico[j] << endl;
+			}
+		}
+	}
+	cout << "Paciente no encontrado" << endl;
+}
+
+void eliminarPaciente(RegistroPacientes &registro, string numeroDNI) {
+    for (int i = 0; i < registro.numPacientes; i++) {
+        if (registro.pacientes[i].numeroDNI == numeroDNI) {
+            for (int j = i; j < registro.numPacientes - 1; j++) {
+                registro.pacientes[j] = registro.pacientes[j + 1];
+            }
+            cout << "Paciente eliminado correctamente" << endl;
+            registro.numPacientes--;
+            return;
+        }
+    }
+    cout << "Paciente no encontrado" << endl;
+}
+
--- gestor_registros.h
+++ gestor_registros.h
@@ -10,8 +10,13 @@ struct RegistroPacientes {
     int numPacientes;
 };
 
-void iniciarRegistro(RegistroPacientes &registro);
-void agregarPaciente(RegistroPacientes &registro, Paciente paciente);
-void listarPacientes(RegistroPacientes registro);
+void iniciarRegistro(RegistroPacientes &registro); // Inicia el registro con 0 pacientes
+void agregarPaciente(RegistroPacientes &registro, Paciente paciente); // Agrega un paciente al registro
+void listarPacientes(RegistroPacientes registro); // Lista todos los pacientes del registro
+void buscarPaciente(RegistroPacientes registro, string numeroDNI); // Busca y devuelve un dato de estructura Paciente o devuelve una estructura vacia
+void actualizarPaciente(RegistroPacientes &registro, string numeroDNI); // Busca y actualiza los datos de un paciente por su numero de DNI en el registro
+void eliminarPaciente(RegistroPacientes &registro, string numeroDNI); // Busca y elimina un dato de estructura Paciente en el registro
+
+
 
 #endif
