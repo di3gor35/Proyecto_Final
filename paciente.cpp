e7582515f568e6c35794c13ae021a5940da8e8cb
Gabriel <gpenalozag@unjbg.edu.pe>
2024-07-28 21:04:44 -0500

    Se implementaron dos funciones para manejar la estructura

--- paciente.cpp
+++ paciente.cpp
@@ -2,6 +2,19 @@
 #include "paciente.h"
 using namespace std;
 
-void mostrarPaciente(Paciente paciente){
-	cout << "El paciente se llama " << paciente.nombre << " y su edad es " << paciente.edad;
+void crearPaciente(Paciente &paciente, string nombre, int edad, string genero, string numeroDNI, string tipoSangre){
+	paciente.nombre = nombre;
+	paciente.edad = edad;
+	paciente.genero = genero;
+	paciente.numeroDNI = numeroDNI;
+	paciente.tipoSangre = tipoSangre;
+	paciente.numEntradasHistorial = 0;
+}
+
+string obtenerPaciente(Paciente &paciente){
+	return paciente.nombre;
+}
+
+void establecerNombre(Paciente &paciente, string nombre){
+	paciente.nombre = nombre;
 }
